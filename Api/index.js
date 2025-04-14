03d095204535a019972e0b886d062538ea799350
BlasterWhite <mguezenn@enssat.fr>
2025-04-15 00:13:35 +0200

    Create API for bicycles

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import db from "./config/db.js";
+import bicycleRouter from "./routes/BicycleRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,11 +14,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-console.log("BD_HOST", process.env.DB_HOST);
-console.log("DB_PORT", process.env.DB_PORT);
-console.log("DB_USER", process.env.DB_USER);
-console.log("DB_PASSWORD", process.env.DB_PASSWORD);
-console.log("DB_NAME", process.env.DB_NAME);
+app.use("/api/bicycles", bicycleRouter);
 
 db.connect((err) => {
   if (err) {
---
+++ Api/routes/BicycleRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  getBicycles,
+  getBicycleById,
+  createBicycle,
+  updateBicycle,
+  deleteBicycle,
+} from "../controllers/Bicycles.js";
+
+const router = express.Router();
+
+// Route to get all bicycles
+router.get("/", getBicycles);
+// Route to get a bicycle by ID
+router.get("/:id", getBicycleById);
+// Route to create a new bicycle
+router.post("/", createBicycle);
+// Route to update a bicycle by ID
+router.put("/:id", updateBicycle);
+// Route to delete a bicycle by ID
+router.delete("/:id", deleteBicycle);
+// Export the router
+export default router;
